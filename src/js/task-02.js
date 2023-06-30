a5d06973fec10f43f62c515cdb9586e2d87834ce
Ludmila-Nowak <126966727+Ludmila-Nowak@users.noreply.github.com>
2023-06-30 19:50:45 +0200

    task 1-3

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listEl = document.querySelector("#ingredients");
+const itemArr = ingredients.map((ingredient) => {
+  const ingredientItemEl = document.createElement("li");
+  ingredientItemEl.classList.add("item");
+  ingredientItemEl.textContent = `${ingredient}`;
+  return ingredientItemEl;
+});
+listEl.append(...itemArr);
