66d8859d9e5377e97f9b9c5061d1c4ded1cd8182
Ludmila-Nowak <126966727+Ludmila-Nowak@users.noreply.github.com>
2023-06-30 20:54:26 +0200

    все видалено

--- src/js/task-01.js
+++
@@ -1,9 +0,0 @@
-const categoriesNumber = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categoriesNumber.length}`);
-
-categoriesNumber.forEach((categories) => {
-  const categoriesName = categories.firstElementChild;
-  console.log(`Category: ${categoriesName.textContent}`);
-  const elementsNumber = categories.lastElementChild.children;
-  console.log(`Elements: ${elementsNumber.length}`);
-});
